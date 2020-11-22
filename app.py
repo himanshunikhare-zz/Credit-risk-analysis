60a06fd1f74876f54b507118862f51e19546f85c
Himanshu Nikhare <himanshunikhare@outlook.com>
2020-11-22 17:26:32 +0530

    file updated

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db =SQLAlchemy(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return redirect('/other-details')
     return render_template('index.html')
 
-@app.route('/other-details')
+@app.route('/other-details', methods=['GET', 'POST'])
 def otherdetails():
+    if request.method == 'POST':
+        return redirect('/result')
     return render_template('form2.html')
 
 @app.route('/result')
