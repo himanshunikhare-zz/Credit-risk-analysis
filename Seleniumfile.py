f3b2186eb500631fd96e159d706b0752a6679fd2
vagrant-coder <himanshunikhare@outlook.com>
2020-11-29 08:19:33 +0530

    selenium added using unittests

--- Seleniumfile.py
+++ Seleniumfile.py
@@ -1,5 +1,3 @@
-# Python program to demonstrate 
-# selenium 
 # selenium script
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,32 +6,40 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 import pandas as pd
 
-data = pd.read_csv('formID.csv')
-data = data.set_index("labelName").T
+def run_selenium(fileAddress):
+    try:
+        data = pd.read_csv(fileAddress)
+        data = data.set_index("labelName").T
+        driver = webdriver.Firefox(executable_path="./geckodriver") 
+        driver.get("http://127.0.0.1:5000/") 
 
-driver = webdriver.Firefox(executable_path="./geckodriver") 
-driver.get("http://127.0.0.1:5000/") 
-print(data)
+    except:
+        return False
 
-for i in data:
-    if data[i]['type'] == "radio":
-        element = driver.find_element_by_id(data[i]['id'])
-        element.click()
-        pass
+    print(data)
+    try:
+        for i in data:
+            if data[i]['type'] == "radio":
+                element = driver.find_element_by_id(data[i]['id'])
+                element.click()
+                pass
 
-    elif data[i]['type'] == "dropdown":
-        element = Select(driver.find_element_by_id(data[i]['id']))
-        element.select_by_visible_text(data[i]['value'])
-        pass
+            elif data[i]['type'] == "dropdown":
+                element = Select(driver.find_element_by_id(data[i]['id']))
+                element.select_by_visible_text(data[i]['value'])
+                pass
 
-    elif data[i]['type'] == "textbox":
-        element = driver.find_element_by_id(data[i]['id'])
-        element.clear()
-        element.send_keys(data[i]['value'])
-        pass
+            elif data[i]['type'] == "textbox":
+                element = driver.find_element_by_id(data[i]['id'])
+                element.clear()
+                element.send_keys(data[i]['value'])
+                pass
 
-    else:
-        print("problem with ",i)
-driver.find_element_by_id('submit-button').click()
-print("exit")
+            else:
+                print("problem with ",i)
+        driver.find_element_by_id('submit-button').click()
+    except:
+        return False
+    return True
 
+run_selenium('formID.csv')
