ae5f222ee4a664901c882bbca44836548cbefb32
vagrantcoder <himanshunikhare@outlook.com>
2020-12-09 18:58:46 +0530

    result rendering issue fixed

--- project/main.py
+++ project/main.py
@@ -12,29 +12,26 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main.route('/profile', methods=['GET', 'POST'])
+@main.route('/profile')
 @login_required
 def profile():
     form = forms.UserForm(request.form)
-
     print(form.errors)
-    if request.method == 'POST':
-        formData = {}
-        for i in request.form:
-            formData[i] = request.form[i]
-        print(formData)
-        pass
-
-    if form.validate():
-        # Save the comment here.
-        return redirect(url_for('result.html'))
-    else:
-        flash('All the form fields are required. ')
-    #if request.method == 'POST':
-        #return redirect('/other-details')
     return render_template('profile.html',values=forms.data,form=form, name=current_user.name)
+
+@main.route('/profile', methods=["POST"])
+@login_required
+def profile_post():
+    print("POST")
+    form = forms.UserForm(request.form)
+    print(form.errors)
+    formData = {}
+    for i in request.form:
+        formData[i] = request.form[i]
+    print(formData)
+    return redirect(url_for('main.result'))
 
 @main.route('/result')
 @login_required
 def result():
-    render_template('result.html')
+    return render_template('result.html')
